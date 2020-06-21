51ad4ac021d3c367f1561f5719447671de20a28c
Om Prakash Singh <ompra.singh@gmail.com>
2020-06-21 15:09:21 +0530

    Initial commit

---
+++ src/test/java/com/bookreview/ratingservice/RatingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookreview.ratingservice;
+
+import org.junit.jupiter.api.Disabled;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingServiceApplicationTests {
+
+    @Disabled
+    void contextLoads() {
+    }
+
+}
